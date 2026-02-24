396298cc2ed48d5b12f7712b1e514d3b90a53573
ST10526428 <ST10526428@myemeris.edu.za>
2026-02-24 12:35:04 +0200

    First Submission

---
+++ src/start_practice/Start_Practice.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package start_practice;
+
+public class Start_Practice {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Hello,\n Welcome\n to\n the\n world\n of\n programming!!!");
+        
+
+    }
+}
---
+++ src/start_practice/Start_and_Go.java
@@ -0,0 +1,28 @@
+
+
+package start_practice;
+
+/**
+ *
+ * @author ST10526428
+ */
+
+public class Start_and_Go {
+
+    public static void main(String[] args) {
+        // displays the first 2 lines of the stop signs
+        System.out.print("\t********\t\t\t********\n      *          *\t\t      *          *\n");
+        //Adds the stop and go text to the sign
+        System.out.print("    *     STOP     *\t\t    *      GO      *\n"); 
+        //Adds the third line of the signs
+        System.out.println("      *          *\t\t      *          *");
+        //Adds the bottom and the rest of the signs structure
+        System.out.print("\t********\t\t\t********\n\t   **\t\t\t\t   **\n\t   ");
+        System.out.print("**\t\t\t\t   **\n\t   **\t\t\t\t   **\n\t   **\t\t\t\t   **\n\t  ****\t\t\t\t  ****\n      ************\t\t      ************\n");
+        //Adds the flooring of the signs
+        System.out.println("|--------------------------------------------------------------|");
+        System.out.println("|--------------------------------------------------------------|");
+
+    }
+}
+               
